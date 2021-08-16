50d0befbab28b05981e4b4084480d658f302a19a
NetFruit <vasilievilay808@gmail.com>
2021-08-16 16:52:35 +0300

    commit message

---
+++ src/main/java/example/com/culinaryranch/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package example.com.culinaryranch.repos;
+
+import example.com.culinaryranch.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
